61f3c61335e60adbae38a7d8bf6ac6bf2520e452
Mirnes <mirnes.kovacevic@gmail.com>
2024-04-09 11:32:01 +0200

    v2.09.04
    Added backdrop blur to navbar.

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -3,7 +3,7 @@ import NavLink from "./NavLink";
 
 const MenuOverlay = ({ links }) => {
   return (
-    <ul className="flex flex-col py-4 items-center justify-center text-xl rounded-b-xl h-[50dvh] bg-[#121212]/100">
+    <ul className="flex flex-col py-4 items-center justify-center text-xl rounded-b-xl h-[50dvh] backdrop-filter backdrop-blur-lg">
       {links.map((link, index) => (
         <li key={index}>
           <NavLink href={link.path} title={link.title} />
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { Link } from 'react-scroll'; // Import Link from react-scroll
+import { Link } from "react-scroll"; // Import Link from react-scroll
 import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
@@ -29,7 +29,7 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed mx-auto border border-none top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-80">
+    <nav className="fixed mx-auto border-b border-purple-500/20 top-0 left-0 right-0 z-10 backdrop-filter backdrop-blur-lg">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           to="home"
